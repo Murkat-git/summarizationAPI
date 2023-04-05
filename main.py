1538dddf5655f4aa49a54da6d0d73990366c510a
Murkat-git <murkat2007@yandex.ru>
2023-04-05 19:45:55 +0500

    Added sentiment analysis

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from transformers import pipeline
+import torch
+import json
 
 app = FastAPI()
 
@@ -12,12 +14,19 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-@app.on_event("startup")
-def load_model():
-    global translator_en_ru, translator_ru_en, summarizer
-    translator_en_ru = pipeline("translation_en_to_ru", model="Helsinki-NLP/opus-mt-en-ru")
-    translator_ru_en = pipeline("translation_ru_to_en", model="Helsinki-NLP/opus-mt-ru-en")
-    summarizer = pipeline("summarization", model="MurkatG/review-summarizer-en")
+
+# translator_en_ru = pipeline("translation_en_to_ru", model="Helsinki-NLP/opus-mt-en-ru")
+# translator_ru_en = pipeline("translation_ru_to_en", model="Helsinki-NLP/opus-mt-ru-en")
+translator_en_ru = pipeline("translation_en_to_ru", model="facebook/wmt19-en-ru")
+translator_ru_en = pipeline("translation_ru_to_en", model="facebook/wmt19-ru-en")
+summarizer = pipeline("summarization", model="MurkatG/review-summarizer-en")
+sentiment_analysis = pipeline(model="cardiffnlp/twitter-roberta-base-sentiment")
+sentiment_labels = {
+    "LABEL_0": "negative",
+    "LABEL_1": "neutral",
+    "LABEL_2": "positive"
+}
+# summarizer = pipeline("summarization", model=r"C:\Users\gtim0\Desktop\models\BART")
 
 
 @app.get('/')
@@ -28,9 +37,15 @@ def index():
 @app.post('/ru')
 async def ru(text: str):
     text = translator_ru_en(text)[0]["translation_text"]
+    sentiment = sentiment_labels[sentiment_analysis(text)[0]["label"]]
     text = summarizer(text)[0]['summary_text']
     text = translator_en_ru(text)[0]['translation_text']
-    return text
+    response = {
+        "summarization": text,
+        "sentiment": sentiment
+    }
+    return response
+
 
 @app.post('/en')
 async def en(text: str):
@@ -40,4 +55,5 @@ async def en(text: str):
 
 if __name__ == '__main__':
     import uvicorn
+
     uvicorn.run(app, port=8000)
